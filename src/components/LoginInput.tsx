77e801be87619db577b2f9df89ccf15798100d0c
leo <hanhyung6376@naver.com>
2022-11-30 17:47:01 +0900

    FEAT: Implement Login Input

--- src/components/LoginInput.tsx
+++ src/components/LoginInput.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
-const LoginInput: FC<Props> = ({ title, type }) => {
+const LoginInput: FC<Props> = (props) => {
+  const { title, type, value, onChange, onBlur, error, message } = props;
   return (
     <>
-      <div>{title}</div>
+      <Text>{title}</Text>
       <TextBox>
-        <TextInput type={type} />
+        <TextInput type={type} value={value} onChange={onChange} onBlur={onBlur} />
       </TextBox>
+      {error ? <Warning>{message}</Warning> : null}
     </>
   );
 };
@@ -17,7 +19,25 @@ export default LoginInput;
 interface Props {
   title: string;
   type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: () => void;
+  error: boolean;
+  message: string;
 }
+
+const Text = styled.p`
+  font-weight: 700;
+  font-size: 13px;
+  color: #6c6c7d;
+`;
+
+const Warning = styled.p`
+  margin-top: 8px;
+  font-weight: 400;
+  font-size: 13px;
+  color: #ed4e5c;
+`;
 
 const TextBox = styled.div`
   margin-top: 8px;
