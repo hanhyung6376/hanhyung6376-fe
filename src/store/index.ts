7c3550c60a40441a9719a16b1633b21deb860ec4
leo <hanhyung6376@naver.com>
2022-12-02 00:21:03 +0900

    FIX: recoil key 난수 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,17 @@
 import { atom } from 'recoil';
+import { v1 } from 'uuid';
 
 export const userAtom = atom<UserType>({
-  key: 'user',
+  key: `user/${v1()}`,
   default: {
     name: null,
+    accessToken: null,
     login: false,
   },
 });
 
 interface UserType {
   name: string | null;
+  accessToken: string | null;
   login: boolean;
 }
