562dc8d728d16e962d568a28e03027c0676d6859
leo <hanhyung6376@naver.com>
2022-12-02 17:13:30 +0900

    FEAT: ssr 방식의 getServerSideProps 구현 서버사이드에서 MSW 사용시 추가 설정이 필요하기 때문에 error 받을 시 api/data/products.json 파일 이용하여 return

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,27 +1,21 @@
-import Link from 'next/link';
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps } from 'next';
 import React from 'react';
 import styled from 'styled-components';
-
+import * as api from 'api';
 import products from '../../api/data/products.json';
+import { Product } from '../../types/product';
+import { numberFormatter } from '../../utilities/product';
 
-const ProductDetailPage: NextPage = () => {
-  const product = products[0];
-
+const ProductDetailPage: NextPage<Props> = ({ thumbnail, name, price, error }) => {
+  if (error) {
+    return <p>존재하지 않는 상품입니다.</p>;
+  }
   return (
     <>
-      <Header>
-        <Link href='/'>
-          <Title>HAUS</Title>
-        </Link>
-        <Link href='/login'>
-          <p>login</p>
-        </Link>
-      </Header>
-      <Thumbnail src={product.thumbnail ? product.thumbnail : '/defaultThumbnail.jpg'} />
+      <Thumbnail src={thumbnail ? thumbnail : '/defaultThumbnail.jpg'} />
       <ProductInfoWrapper>
-        <Name>{product.name}</Name>
-        <Price>{product.price}원</Price>
+        <Name>{name}</Name>
+        <Price>{numberFormatter(price)}원</Price>
       </ProductInfoWrapper>
     </>
   );
@@ -29,16 +23,39 @@ const ProductDetailPage: NextPage = () => {
 
 export default ProductDetailPage;
 
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-`;
+export const getServerSideProps: GetServerSideProps<Props, { id: string }> = async (context) => {
+  const id = context.params?.id || '';
+  const res = await api.getProduct(id);
 
-const Title = styled.a`
-  font-size: 48px;
-`;
+  let result: Props = {
+    id: '',
+    thumbnail: null,
+    name: '',
+    price: 0,
+    error: false,
+  };
+  if (res.error) {
+    const index = parseInt(id);
+    if (index && index <= products.length) {
+      result = { ...products[index], error: false };
+    } else {
+      result = { ...result, error: true };
+    }
+  } else {
+    const { data } = res.data;
+    const { thumbnail, name, price, id } = data;
+    result = { thumbnail, name, price, id, error: false };
+  }
+  return {
+    props: result,
+  };
+};
+
+interface Props extends Product {
+  error: boolean;
+}
+
+const ErrorMessage = styled.div``;
 
 const Thumbnail = styled.img`
   width: 100%;
