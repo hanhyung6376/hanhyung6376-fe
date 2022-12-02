78f83cc7a391004940773e74326b20c8b57b72e4
leo <hanhyung6376@naver.com>
2022-12-03 02:50:29 +0900

    FEAT: data fetching 및 error 처리  구현

--- src/pages/pagination.tsx
+++ src/pages/pagination.tsx
@@ -1,21 +1,59 @@
-import { useRouter } from 'next/router';
-import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-import products from '../api/data/products.json';
+import { useRouter } from 'next/router';
 import ProductList from '../components/ProductList';
 import Pagination from '../components/Pagination';
+import usePagination from '../hooks/usePagination';
+import Error from 'components/common/Error';
+import { useRecoilState } from 'recoil';
+import { pageAtom } from '../store';
+import * as api from '../api';
+import { Product } from '../types/product';
 
 const PaginationPage: NextPage = () => {
   const router = useRouter();
-  const { page } = router.query;
+  const params = router.query.page;
+  const [page, setPage] = useRecoilState(pageAtom);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState<Product[]>([]);
+  const [total, setTotal] = useState<number>(0);
 
+  const fetchData = async () => {
+    const res = await api.getProducts({ page, size: 10 });
+
+    if (res.error) {
+      setError(true);
+    } else {
+      const { products, totalCount } = res.data.data;
+      setData(products);
+      setTotal(Math.ceil(totalCount / 10));
+      setError(false);
+    }
+  };
+
+  useEffect(() => {
+    const number = Number(params as string);
+    if (Number.isInteger(number)) {
+      setPage(number);
+    } else {
+      setError(true);
+    }
+  }, [params]);
+
+  useEffect(() => {
+    if (page > 0) {
+      fetchData();
+    }
+  }, [page]);
+
+  if (error) {
+    return <Error message='존재하지 않는 페이지입니다.' />;
+  }
   return (
     <Container>
-      <ProductList products={products.slice(0, 10)} />
-      <Pagination />
+      <ProductList products={data} />
+      <Pagination totalPage={total} />
     </Container>
   );
 };
