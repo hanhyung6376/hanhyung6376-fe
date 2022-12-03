c1c08cd2bdd622ac4abf456e76d0646b25e49d6a
leo <hanhyung6376@naver.com>
2022-12-03 14:54:50 +0900

    FEAT: useInfiniteScroll 적용

--- src/pages/infinite-scroll.tsx
+++ src/pages/infinite-scroll.tsx
@@ -1,15 +1,48 @@
 import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
-
-import products from '../api/data/products.json';
 import ProductList from '../components/ProductList';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
+import * as api from 'api';
+import { Product } from '../types/product';
+
+const NUMBER = 16;
 
 const InfiniteScrollPage: NextPage = () => {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState<Product[]>([]);
+  const [total, setTotal] = useState();
+  const [hasNext, setHasNext] = useState(true);
+  const fetchMoreEl = useRef(null);
+  const intersecting = useInfiniteScroll(fetchMoreEl);
+
+  const fetchData = async () => {
+    const res = await api.getProducts({ page, size: NUMBER });
+    const { products, totalCount } = res.data.data;
+    setData([...data, ...products]);
+    if (!total) {
+      setTotal(totalCount);
+    }
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    if (intersecting && hasNext) {
+      fetchData();
+    }
+  }, [intersecting]);
+
+  useEffect(() => {
+    if (data.length === total) {
+      setHasNext(false);
+    }
+  }, [data]);
+
   return (
     <Container>
-      <ProductList products={products} />
+      <ProductList products={data} />
+      <div ref={fetchMoreEl} />
     </Container>
   );
 };
