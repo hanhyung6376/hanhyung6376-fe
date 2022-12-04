86c00dd2909c81ca0f73fc4ee1aa0128ae3c7e48
leo <hanhyung6376@naver.com>
2022-12-05 00:28:27 +0900

    FEAT: 무한 스크롤 관련 상태관리 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
 import { atom } from 'recoil';
 import { v1 } from 'uuid';
+import { Product } from '../types/product';
 
-export const userAtom = atom<UserType>({
+export const userAtom = atom<User>({
   key: `user/${v1()}`,
   default: {
     name: null,
@@ -10,13 +11,30 @@ export const userAtom = atom<UserType>({
   },
 });
 
-export const pageAtom = atom({
+export const pageAtom = atom<number>({
   key: `page/${v1()}`,
   default: 0,
 });
 
-interface UserType {
+export const infiniteAtom = atom<Infinite>({
+  key: `scroll/${v1()}`,
+  default: {
+    scroll: 0,
+    page: 1,
+    back: false,
+    product: [],
+  },
+});
+
+interface User {
   name: string | null;
   accessToken: string | null;
   login: boolean;
+}
+
+interface Infinite {
+  scroll: number;
+  back: boolean;
+  product: Product[];
+  page: number;
 }
